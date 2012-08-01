0a93bbdd574d6950c1e49f7c30d2d4ba094a5583
Alex Delegard <adelegard@gmail.com>
2012-08-01 10:49:12 -0500

    # followers changes as you follow/unfollow.

--- app/assets/javascripts/user_follows.js
+++ app/assets/javascripts/user_follows.js
@@ -6,10 +6,16 @@ $(function() {
         $.ajax({
         url: href,
         success: function(){
+            var followers = $('.stats .followers');
+            var num_followers = parseInt(followers.text());
             if (self.hasClass("unfollow")) {
                 self.prev(".follow").addClass("dib").show();
+                num_followers = num_followers - 1;
+                followers.text(num_followers);
             } else {
                 self.next(".unfollow").addClass("dib").show();
+                num_followers = num_followers + 1;
+                followers.text(num_followers);
             }
             self.hide();
         }
