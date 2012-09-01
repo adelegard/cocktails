3c8435027efb3c8c364fde5dcb42598b2c4fa3dc
Alex Delegard <adelegard@gmail.com>
2012-09-01 18:36:26 -0500

    Making our implementation of expander.js awesomer

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -11,6 +11,29 @@ $(function() {
       // change from display 'none' to 'block' so the onSlice method
       // doesn't make the overflowing element make the page jump
       $(this).css('display', 'block');
+    },
+    beforeExpand: function() {
+      $(this).addClass('expanded');
+    },
+    afterExpand: function() {
+      $('html').on('click', function() {
+        //invoke the 'read-less' click to collapse the content
+        $('.read-less a').trigger('click');
+      });
+      $(document).on('keydown', function(e) {
+        if (e.which === 27) { // escape key
+           $('.read-less a').trigger('click');
+         }
+      });
+      $('.expandable.expanded').on('click', function(e) {
+        e.stopPropagation();
+      });
+    },
+    onCollapse: function() {
+      $(this).removeClass('expanded');
+      $('html').off('click');
+      $(document).off('keydown');// This kills all keydown's. Not ideal.
+      $('.expandable.expanded').off('click');
     }
   });
 
